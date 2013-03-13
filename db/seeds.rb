818e91ada8b93c7873a8b66e8d4c8f48d87c5207
James Hunt <ohthatjames@googlemail.com>
2013-03-13 12:09:16 +0000

    Basic category

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Category.create([{:name => "Category 1"}, {:name => "Category 2"}, {:name => "Category 3"}])

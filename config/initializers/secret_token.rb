899483e98af660bd9c9c09cbb146760ec8ee46f2
James Hunt <ohthatjames@googlemail.com>
2013-03-13 11:53:11 +0000

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NothsCategoryPages::Application.config.secret_token = '9b9f02ee1dacd27f17b74a427bbbc7211b7b0d9078490dcd88f6ac6b25bd9554a0b2464975d6e57216fa29c06f6f9b160d595239233fcdeb1a4437ff54c37637'
